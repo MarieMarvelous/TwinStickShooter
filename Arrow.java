32ea9d43feb8164b1ee0c469ad065fbeb57d19d5
Marie Rogowski <marie.rogowski@stud.hs-hannover>
2019-10-09 01:06:25 +0200

    Switched to projectile as class that arrow extends from

--- Arrow.java
+++ Arrow.java
@@ -6,7 +6,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Arrow extends Weapons
+public class Arrow extends Projectile
 {
     /**
      * Act - do whatever the Arrow wants to do. This method is called whenever
